0f73f2032337217646248723852714ce095e5877
ewelina-borucka <ewelina.borucka@gmail.com>
2017-01-25 08:58:22 +1100

    add content and animation for portfolio page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log($);
+// -----------------------------------------------------------------------//
+// ------------------------------  TweenMax  ------------------------------//
+// -----------------------------------------------------------------------//
+
+
+
+
+
+
+// --------------------  Portfolio Page  -----------------------//
+
+var tl = new TimelineMax({reapet: 1, reapetDelay: 1});
+tl.staggerTo(".box", 1, {y: 40, ease: Bounce.easeOut}, 0.2);
+
+
+
+
+// ------------------------  Info Page  -----------------------//
+
+TweenLite.set(".secondary", {visibility: "visible"});
+var tl = new TimelineMax();
+
+tl.staggerFrom(".skills li", 0.9, {
+  opacity: 0,
+  cycle: {
+    rotationX: [-90, 90],
+    transformOrigin: ["50% top -100", "50% bottom 100"]
+  }
+}, 0.1);
+
+
+
+// ------------------------  Home Page  -----------------------//
